1d18d7d5ed8c809096264b6c55fc7573cae22b11
Alexandra-Myers <bia416cat@gmail.com>
2022-08-04 22:21:59 -0400

    commit

--- src/main/java/net/alexandra/atlas/atlas_combat/item/IAttribute.java
+++ src/main/java/net/alexandra/atlas/atlas_combat/item/IAttribute.java
@@ -3,12 +3,25 @@ package net.alexandra.atlas.atlas_combat.item;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 
 public interface IAttribute {
-	static Attribute getAttribute(String name){
-		return Registry.ATTRIBUTE.get(new ResourceLocation(name));
+	static Attribute getNewAttackSpeed() {
+		return NEW_ATTACK_SPEED;
 	}
-	static Attribute getAttribute(ResourceLocation id){
-		return Registry.ATTRIBUTE.get(id);
+	static Attribute getAttackReach() {
+		return ATTACK_REACH;
 	}
+	private static Attribute registrySet(String var0, Attribute var1, Attribute var2){
+		return Registry.registerMapping(Registry.ATTRIBUTE, Registry.ATTRIBUTE.getId(var2), var0, var1);
+	}
+
+	private static Attribute register(String id, Attribute attribute) {
+		return Registry.register(Registry.ATTRIBUTE, id, attribute);
+	}
+
+	Attribute NEW_ATTACK_SPEED = registrySet("generic.attack_speed", (new RangedAttribute("attribute.name.generic.attack_speed", 4.0, 0.10000000149011612, 1024.0)).setSyncable(true), Attributes.ATTACK_SPEED);
+
+	Attribute ATTACK_REACH = register("generic.attack_reach", (new RangedAttribute("attribute.name.generic.attack_reach", 2.5, 0.0, 6.0)).setSyncable(true));
 }
--- src/main/java/net/alexandra/atlas/atlas_combat/mixin/AttributesMixin.java
+++ src/main/java/net/alexandra/atlas/atlas_combat/mixin/AttributesMixin.java
@@ -2,29 +2,14 @@ package net.alexandra.atlas.atlas_combat.mixin;
 
 import net.alexandra.atlas.atlas_combat.item.IAttribute;
 import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.Desc;
 
 @Mixin(Attributes.class)
 public class AttributesMixin implements IAttribute {
-	@Shadow
-	private static Attribute register(String var0, Attribute var1) {
-		return null;
-	}
-
-	@Shadow
-	@Final
-	public static Attribute ATTACK_SPEED;
-
-	private static Attribute registrySet(String var0, Attribute var1, Attribute var2){
-		return Registry.registerMapping(Registry.ATTRIBUTE, Registry.ATTRIBUTE.getId(var2), var0, var1);
-	}
-
-	static final Attribute NEW_ATTACK_SPEED = registrySet("generic.attack_speed", (new RangedAttribute("attribute.name.generic.attack_speed", 4.0, 0.10000000149011612, 1024.0)).setSyncable(true), ATTACK_SPEED);
-
-	static final Attribute ATTACK_REACH = register("generic.attack_reach", (new RangedAttribute("attribute.name.generic.attack_reach", 2.5, 0.0, 6.0)).setSyncable(true));
 }
