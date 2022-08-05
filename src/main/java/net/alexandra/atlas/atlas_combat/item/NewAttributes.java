e2300564de477e52907ff6f8d90fee33d63e5544
Alexandra-Myers <bia416cat@gmail.com>
2022-08-05 12:02:46 -0400

    commit

--- src/main/java/net/alexandra/atlas/atlas_combat/item/NewAttributes.java
+++ src/main/java/net/alexandra/atlas/atlas_combat/item/NewAttributes.java
@@ -6,15 +6,12 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 
 public class NewAttributes extends Attributes{
-	private static Attribute registrySet(String var0, Attribute var1, Attribute var2){
-		return Registry.registerMapping(Registry.ATTRIBUTE, Registry.ATTRIBUTE.getId(var2), var0, var1);
-	}
 
 	private static Attribute register(String id, Attribute attribute) {
 		return Registry.register(Registry.ATTRIBUTE, id, attribute);
 	}
 
-	public static final Attribute NEW_ATTACK_SPEED = registrySet("generic.attack_speed", (new RangedAttribute("attribute.name.generic.attack_speed", 4.0, 0.10000000149011612, 1024.0)).setSyncable(true), ATTACK_SPEED);
+	public static final Attribute NEW_ATTACK_SPEED = register("generic.new_attack_speed", (new RangedAttribute("attribute.name.generic.new_attack_speed", 4.0, 0.10000000149011612, 1024.0)).setSyncable(true));
 
 	public static final Attribute ATTACK_REACH = register("generic.attack_reach", (new RangedAttribute("attribute.name.generic.attack_reach", 2.5, 0.0, 6.0)).setSyncable(true));
 }
