cb046ecf8b47a8b7e2b5f1e3f6d6420db0115e33
Alexandra-Myers <bia416cat@gmail.com>
2022-08-06 14:15:55 -0400

    commit

---
+++ src/main/java/net/alexandra/atlas/atlas_combat/mixin/TridentItemMixin.java
@@ -0,0 +1,30 @@
+package net.alexandra.atlas.atlas_combat.mixin;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.alexandra.atlas.atlas_combat.item.WeaponType;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.item.*;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(TridentItem.class)
+public class TridentItemMixin extends Item implements Vanishable {
+	@Shadow
+	private Multimap<Attribute, AttributeModifier> defaultModifiers;
+
+	public TridentItemMixin(Item.Properties properties) {
+		super(properties);
+	}
+
+	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMultimap$Builder;build()Lcom/google/common/collect/ImmutableMultimap;"))
+	public ImmutableMultimap test(ImmutableMultimap.Builder instance) {
+		ImmutableMultimap.Builder var3 = ImmutableMultimap.builder();
+		WeaponType.TRIDENT.addCombatAttributes(Tiers.NETHERITE, var3);
+		return var3.build();
+	}
+}
