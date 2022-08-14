866967ebf42e6731477e157e3d06bd0400a4fd33
Alexandra-Myers <bia416cat@gmail.com>
2022-08-14 13:19:22 -0400

    fixed something

---
+++ src/main/java/net/alexandra/atlas/atlas_combat/mixin/EntityMixin.java
@@ -0,0 +1,16 @@
+package net.alexandra.atlas.atlas_combat.mixin;
+
+import net.alexandra.atlas.atlas_combat.extensions.PlayerExtensions;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+	@ModifyConstant(
+		method = "getFireImmuneTicks", constant = @Constant(intValue = 1))
+	private int correctFireImmuneTicks(int constant) {
+		return 3;
+	}
+}
