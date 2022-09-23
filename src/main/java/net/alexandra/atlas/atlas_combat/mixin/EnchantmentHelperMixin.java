2c8ad30e91b60ab4699ba2300bdfc4033367eaf2
Alexandra-Myers <bia416cat@gmail.com>
2022-09-23 10:32:11 -0400

    fixed cleaving

--- src/main/java/net/alexandra/atlas/atlas_combat/mixin/EnchantmentHelperMixin.java
+++ src/main/java/net/alexandra/atlas/atlas_combat/mixin/EnchantmentHelperMixin.java
@@ -1,18 +1,32 @@
 package net.alexandra.atlas.atlas_combat.mixin;
 
+import net.alexandra.atlas.atlas_combat.enchantment.CleavingEnchantment;
 import net.alexandra.atlas.atlas_combat.extensions.IEnchantmentHelper;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobType;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Iterator;
+import java.util.List;
 
 import static net.minecraft.world.item.enchantment.EnchantmentHelper.getItemEnchantmentLevel;
 
 @Mixin(EnchantmentHelper.class)
 public abstract class EnchantmentHelperMixin implements IEnchantmentHelper {
+	private static Enchantment currentEnchantment;
+	private static ItemStack itemStack;
 	@Shadow
 	public static float getDamageBonus(ItemStack par1, MobType par2) {
 		return 0;
@@ -44,6 +58,19 @@ public abstract class EnchantmentHelperMixin implements IEnchantmentHelper {
 			}
 
 			return i;
+		}
+	}
+	@Inject(method = "getAvailableEnchantmentResults", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/Enchantment;isTreasureOnly()Z"), locals = LocalCapture.CAPTURE_FAILHARD)
+	private static void extractEnchantment(int power, ItemStack stack, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentInstance>> cir, List<EnchantmentInstance> list, Item item, boolean bl, Iterator iterator, Enchantment enchantment) {
+		currentEnchantment = enchantment;
+		itemStack = stack;
+	}
+	@Redirect(method = "getAvailableEnchantmentResults", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentCategory;canEnchant(Lnet/minecraft/world/item/Item;)Z"))
+	private static boolean redirectCanEnchant(EnchantmentCategory instance, Item item) {
+		if(currentEnchantment instanceof CleavingEnchantment && itemStack != null) {
+			return currentEnchantment.canEnchant(itemStack);
+		}else {
+			return instance.canEnchant(item);
 		}
 	}
 }
