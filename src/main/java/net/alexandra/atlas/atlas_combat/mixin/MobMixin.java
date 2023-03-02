d2e15903be25dd14609190ee363f5db74993b5b0
Alexandra-Myers <bia416cat@gmail.com>
2023-03-02 10:31:32 -0500

    match Forge changes

--- src/main/java/net/alexandra/atlas/atlas_combat/mixin/MobMixin.java
+++ src/main/java/net/alexandra/atlas/atlas_combat/mixin/MobMixin.java
@@ -1,30 +1,16 @@
 package net.alexandra.atlas.atlas_combat.mixin;
 
-import net.alexandra.atlas.atlas_combat.extensions.IMob;
 import net.alexandra.atlas.atlas_combat.extensions.LivingEntityExtensions;
-import net.alexandra.atlas.atlas_combat.util.MobInventory;
-import net.minecraft.core.GlobalPos;
-import net.minecraft.stats.Stats;
-import net.minecraft.util.Mth;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.Optional;
-
 @Mixin(Mob.class)
-public abstract class MobMixin extends LivingEntity implements IMob {
+public abstract class MobMixin extends LivingEntity {
 
 	protected MobMixin(EntityType<? extends LivingEntity> entityType, Level level) {
 		super(entityType, level);
--- src/main/java/net/alexandra/atlas/atlas_combat/player/IPlayer.java
+++
@@ -1,7 +0,0 @@
-package net.alexandra.atlas.atlas_combat.player;
-
-public interface IPlayer {
-	void attackAir();
-
-	void customDisableShield(float damage);
-}
