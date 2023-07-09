bd69495346fe146567cbdb741bf45da8239dd84e
Alexandra-Myers <bia416cat@gmail.com>
2023-07-08 22:55:29 -0400

    rarity update

--- src/main/java/net/alexandra/atlas/atlas_combat/enchantment/DefendingEnchantment.java
+++ src/main/java/net/alexandra/atlas/atlas_combat/enchantment/DefendingEnchantment.java
@@ -6,7 +6,6 @@ import net.alexandra.atlas.atlas_combat.extensions.IShieldItem;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TieredItem;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
