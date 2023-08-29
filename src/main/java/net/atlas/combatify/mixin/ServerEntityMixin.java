544bcb5d008f5450f954d4c75ce5e81333efe370
Alexandra-Myers <bia416cat@gmail.com>
2023-08-28 22:14:47 -0400

    do something cursed

--- src/main/java/net/atlas/combatify/mixin/ServerEntityMixin.java
+++ src/main/java/net/atlas/combatify/mixin/ServerEntityMixin.java
@@ -1,37 +1,43 @@
 package net.atlas.combatify.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.atlas.combatify.extensions.IUpdateAttributesPacket;
+import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundBundlePacket;
 import net.minecraft.network.protocol.game.ClientboundUpdateAttributesPacket;
 import net.minecraft.server.level.ServerEntity;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerEntity.class)
-public class ServerEntityMixin {
+public abstract class ServerEntityMixin {
 	@Shadow
 	@Final
 	private Entity entity;
 
-	@ModifyExpressionValue(method = "addPairing", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/game/ClientboundBundlePacket;<init>(Ljava/lang/Iterable;)V"))
-	public ClientboundBundlePacket modifyAttributes(ClientboundBundlePacket original, @Local(ordinal = 0) ServerPlayer serverPlayer) {
-		original.subPackets().forEach(clientGamePacketListenerPacket -> {
-			if(clientGamePacketListenerPacket instanceof ClientboundUpdateAttributesPacket clientboundUpdateAttributesPacket) {
-				((IUpdateAttributesPacket) clientboundUpdateAttributesPacket).changeAttributes(serverPlayer);
-			}
-		});
-		return original;
+	@Shadow
+	protected abstract void broadcastAndSend(Packet<?> packet);
+
+	@Redirect(method = "addPairing", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;send(Lnet/minecraft/network/protocol/Packet;)V"))
+	public void modifyAttributes(ServerGamePacketListenerImpl instance, Packet<?> packet, @Local(ordinal = 0) ServerPlayer serverPlayer) {
+		if(packet instanceof ClientboundBundlePacket clientboundBundlePacket)
+			clientboundBundlePacket.subPackets().forEach(clientGamePacketListenerPacket -> {
+				if(clientGamePacketListenerPacket instanceof ClientboundUpdateAttributesPacket clientboundUpdateAttributesPacket) {
+					((IUpdateAttributesPacket) clientboundUpdateAttributesPacket).changeAttributes(serverPlayer);
+				}
+			});
+		instance.send(packet);
 	}
-	@ModifyExpressionValue(method = "sendDirtyEntityData", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/game/ClientboundUpdateAttributesPacket;<init>(ILjava/util/Collection;)V"))
-	public ClientboundUpdateAttributesPacket modifyAttributes1(ClientboundUpdateAttributesPacket original) {
-		if(entity instanceof ServerPlayer serverPlayer)
-			((IUpdateAttributesPacket) original).changeAttributes(serverPlayer);
-		return original;
+	@Redirect(method = "sendDirtyEntityData", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerEntity;broadcastAndSend(Lnet/minecraft/network/protocol/Packet;)V"))
+	public void modifyAttributes1(ServerEntity instance, Packet<?> packet) {
+		if(entity instanceof ServerPlayer serverPlayer && packet instanceof ClientboundUpdateAttributesPacket clientboundUpdateAttributesPacket)
+			((IUpdateAttributesPacket) clientboundUpdateAttributesPacket).changeAttributes(serverPlayer);
+		broadcastAndSend(packet);
 	}
 }
