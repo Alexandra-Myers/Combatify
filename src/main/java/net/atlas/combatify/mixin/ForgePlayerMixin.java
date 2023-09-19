d95990c93243cc9ee4b9feb6442b310a0d8070c6
Alexandra-Myers <bia416cat@gmail.com>
2023-09-19 18:40:48 -0400

    Release Candidate 2 Forge - incompat incoming

--- src/main/java/net/atlas/combatify/mixin/ForgePlayerMixin.java
+++ src/main/java/net/atlas/combatify/mixin/ForgePlayerMixin.java
@@ -1,20 +1,19 @@
 package net.atlas.combatify.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.atlas.combatify.extensions.PlayerExtensions;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.extensions.IForgePlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(IForgePlayer.class)
 public interface ForgePlayerMixin {
 	@Shadow
 	Player self();
 
-	@ModifyReturnValue(method = "getEntityReach", at = @At(value = "RETURN"), remap = false)
-	default double modReach(double original) {
+	@Overwrite(remap = false)
+	default double getEntityReach() {
 		return ((PlayerExtensions)self()).getCurrentAttackReach(0.0F);
 	}
 }
