90a541bcb389b08f34e635dabe46f5114793a4e8
Alexandra-Myers <bia416cat@gmail.com>
2023-09-21 13:07:35 -0400

    Release Candidate 2 Forge - Oops

--- src/main/java/net/atlas/combatify/config/ConfigSynchronizer.java
+++ src/main/java/net/atlas/combatify/config/ConfigSynchronizer.java
@@ -14,12 +14,9 @@ import net.minecraft.network.Connection;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,23 +31,11 @@ public class ConfigSynchronizer {
 
 	}
 
-    /**
-     * Retrieve the options which the given player's client
-     * sent to the server during config synchronization
-     *
-     * @param player     The player for which to retrieve the client values
-     * @return The player's client's values of the given config options,
-     * or {@code null} if no config with the given name was synced
-     */
-    public static @Nullable Map<String, SynchableOption<?>> getClientOptions(ServerPlayer player) {
-		return CLIENT_OPTION_STORAGE.get(((ServerGamePacketListenerAccessor) player.connection).combatify$getConnection());
-    }
-
     public static void write(FriendlyByteBuf packet, int ordinal) {
-        packet.writeVarInt(1);
-
         var configBuf = new FriendlyByteBuf(Unpooled.buffer());
         var optionBuf = new FriendlyByteBuf(Unpooled.buffer());
+
+		configBuf.writeVarInt((int) Combatify.CONFIG.options.values().stream().filter(option -> 2 >= ordinal).count());
 
         Combatify.CONFIG.options.forEach((key, option) -> {
             if (2 < ordinal) return;
@@ -64,7 +49,6 @@ public class ConfigSynchronizer {
             configBuf.writeBytes(optionBuf);
         });
 
-        packet.writeVarInt(configBuf.readableBytes());
         packet.writeBytes(configBuf);
     }
 
@@ -92,7 +76,6 @@ public class ConfigSynchronizer {
     @OnlyIn(Dist.CLIENT)
     public static void applyClient(FriendlyByteBuf buf) {
 		Minecraft client = Minecraft.getInstance();
-		ClientPacketListener handler = client.getConnection();
         Combatify.LOGGER.info("Applying server overrides");
         var mismatchedOptions = new HashMap<SynchableOption<?>, Object>();
 
@@ -104,11 +87,12 @@ public class ConfigSynchronizer {
 
             if (!mismatchedOptions.isEmpty()) {
                 Combatify.LOGGER.error("Aborting connection, non-syncable config values were mismatched");
-                mismatchedOptions.forEach((option, serverValue) -> LogUtils.getLogger().error("- Option {} in config '{}' has value '{}' but server requires '{}'",
-						Combatify.CONFIG.options.inverse().get(option), "combatify-config", option.get(), serverValue));
+                mismatchedOptions.forEach((option, serverValue) -> LogUtils.getLogger().error("- Option {} in config 'combatify-config' has value '{}' but server requires '{}'",
+						Combatify.CONFIG.options.inverse().get(option), option.get(), serverValue));
 				MutableComponent error = Component.literal("Aborting connection, non-syncable config values were mismatched.\n");
 				mismatchedOptions.forEach(((option, o) -> error.append(Component.literal(Combatify.CONFIG.options.inverse().get(option) + " does not match the server's value of " + o + ".\n"))));
-                handler.getConnection().disconnect(error);
+				if(client.player != null)
+					client.player.connection.getConnection().disconnect(error);
                 return;
             }
         }
