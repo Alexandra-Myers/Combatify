260fad861962f02728fcfa241a48d8f6dcf5d1ab
Alexandra-Myers <bia416cat@gmail.com>
2023-09-24 16:12:39 -0400

    Release Candidate 5 Forge - Refcator

--- src/main/java/net/atlas/combatify/mixin/EntityMixin.java
+++ src/main/java/net/atlas/combatify/mixin/EntityMixin.java
@@ -1,16 +1,24 @@
 package net.atlas.combatify.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.atlas.combatify.Combatify;
 import net.atlas.combatify.util.MethodHandler;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
-public class EntityMixin {
-	@ModifyReturnValue(method = "getBoundingBox", at = @At(value = "RETURN"))
-	public AABB inflateBoxes(AABB original) {
-		return MethodHandler.inflateBoundingBox(original);
+public abstract class EntityMixin {
+
+	@Shadow
+	private AABB bb;
+
+	@Inject(method = "setBoundingBox", at = @At(value = "RETURN"))
+	public void inflateBoxes(AABB aABB, CallbackInfo ci) {
+		if (aABB.getSize() < Combatify.CONFIG.minHitboxSize.get())
+			bb = MethodHandler.inflateBoundingBox(aABB);
 	}
 }
