010946d711966c185339d286909d52c12c98ab7f
Alexandra-Myers <bia416cat@gmail.com>
2024-12-25 18:32:33 -0500

    Combatify 1.2.5 1.21.4 Beta 3 Fabric - forgor

--- src/main/java/net/atlas/combatify/util/blocking/effect/HurtEntity.java
+++ src/main/java/net/atlas/combatify/util/blocking/effect/HurtEntity.java
@@ -1,6 +1,5 @@
 package net.atlas.combatify.util.blocking.effect;
 
-import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Holder;
@@ -13,25 +12,26 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.enchantment.EnchantedItemInUse;
+import net.minecraft.world.item.enchantment.LevelBasedValue;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.Map;
 
-public record HurtEntity(Holder<DamageType> damageType, float amount) implements PostBlockEffect {
+public record HurtEntity(Holder<DamageType> damageType, LevelBasedValue amount) implements PostBlockEffect {
 	public static final ResourceLocation ID = ResourceLocation.withDefaultNamespace("hurt_entity");
 
 	public static final MapCodec<HurtEntity> MAP_CODEC = RecordCodecBuilder.mapCodec(instance ->
 		instance.group(DamageType.CODEC.fieldOf("damage_type").forGetter(HurtEntity::damageType),
-				Codec.FLOAT.fieldOf("damage").forGetter(HurtEntity::amount))
+				PostBlockEffects.LEVEL_BASED_VALUE_OR_CONSTANT_CODEC.fieldOf("damage").forGetter(HurtEntity::amount))
 			.apply(instance, HurtEntity::new)
 	);
 	public static final StreamCodec<RegistryFriendlyByteBuf, HurtEntity> STREAM_CODEC = StreamCodec.composite(DamageType.STREAM_CODEC, HurtEntity::damageType,
-		ByteBufCodecs.FLOAT, HurtEntity::amount,
+		ByteBufCodecs.fromCodecTrusted(PostBlockEffects.LEVEL_BASED_VALUE_OR_CONSTANT_CODEC), HurtEntity::amount,
 		HurtEntity::new);
 
 	@Override
 	public void doEffect(ServerLevel serverLevel, EnchantedItemInUse enchantedItemInUse, LivingEntity attacker, DamageSource damageSource, int enchantmentLevel, LivingEntity toApply, Vec3 position) {
-		if (!damageSource.combatify$originatedFromBlockedAttack()) toApply.hurtServer(serverLevel, new DamageSource(damageType, enchantedItemInUse.owner()).combatify$originatesFromBlockedAttack(true), amount);
+		if (!damageSource.combatify$originatedFromBlockedAttack()) toApply.hurtServer(serverLevel, new DamageSource(damageType, enchantedItemInUse.owner()).combatify$originatesFromBlockedAttack(true), amount.calculate(enchantmentLevel));
 	}
 
 	@Override
