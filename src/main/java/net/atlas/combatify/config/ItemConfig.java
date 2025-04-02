a31cdf37cb02a63d3e60fdc0b86749ab46b0c378
Alexandra-Myers <bia416cat@gmail.com>
2025-04-02 15:16:28 -0400

    Combatify 1.21.1 1.2.6 Fabric - I forgor

--- src/main/java/net/atlas/combatify/config/ItemConfig.java
+++ src/main/java/net/atlas/combatify/config/ItemConfig.java
@@ -14,8 +14,6 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
-import net.minecraft.CrashReport;
-import net.minecraft.ReportedException;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
@@ -27,14 +25,10 @@ import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.TagKey;
-import net.minecraft.util.Mth;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.*;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 
 import java.io.InputStream;
 import java.util.*;
@@ -275,149 +269,6 @@ public class ItemConfig extends AtlasConfig {
 				", tagKeys=" + tagKeys +
 				", configurableData=" + configurableData +
 				'}';
-		}
-	}
-	public record Formula(String armour, String enchantment) {
-		public Formula(String written) {
-			this(written.split("enchant:", 2)[0], written.split("enchant:", 2)[1]);
-		}
-		public String written() {
-			return armour + "enchant:" + enchantment;
-		}
-		public static final Codec<Formula> SINGLE_LINE = Codec.STRING.xmap(Formula::new, Formula::written);
-		public static final Codec<Formula> CONCAT_OBJECT = RecordCodecBuilder.create(instance ->
-			instance.group(Codec.STRING.fieldOf("armor_protection").forGetter(Formula::armour),
-				Codec.STRING.fieldOf("enchantment_protection").forGetter(Formula::enchantment))
-				.apply(instance, Formula::new));
-		public static final Codec<Formula> CODEC = Codec.withAlternative(CONCAT_OBJECT, SINGLE_LINE);
-		public float armourCalcs(float amount, DamageSource damageSource, float armour, float armourToughness) {
-			String armourFormula = this.armour;
-			armourFormula = armourFormula.replaceAll("D", String.valueOf(amount)).replaceAll("P", String.valueOf(armour)).replaceAll("T", String.valueOf(armourToughness));
-			float result = solveFormula(armourFormula);
-			ItemStack itemStack = damageSource.getWeaponItem();
-			if (itemStack != null && damageSource.getEntity().level() instanceof ServerLevel serverLevel)
-				result = 1 - Mth.clamp(EnchantmentHelper.modifyArmorEffectiveness(serverLevel, itemStack, damageSource.getEntity(), damageSource, result), 0.0F, 1.0F);
-			else result = 1 - result;
-			return amount * result;
-		}
-		public float enchantCalcs(float amount, float enchantLevel) {
-			String enchantFormula = this.enchantment;
-			enchantFormula = enchantFormula.replaceAll("D", String.valueOf(amount)).replaceAll("E", String.valueOf(enchantLevel));
-			float result = solveFormula(enchantFormula);
-			return amount * result;
-		}
-		public float solveFormula(String formula) {
-			if (!formula.contains("(") && !formula.contains("[") && !formula.contains("<") && !formula.contains("{"))
-				return solveInner(formula);
-			float res;
-			String par = formula;
-			while (par.contains("(") || par.contains("[") || par.contains("<") || par.contains("{")) {
-				par = simplifyParenthesis(par, 0);
-			}
-			res = solveInner(par);
-			return res;
-		}
-		public String simplifyParenthesis(String par, int recursion) {
-			if (recursion > 10)
-				throw new ReportedException(CrashReport.forThrowable(new IllegalStateException("Cannot have more than 10 operations inside of each other!"), "Handling armour calculations"));
-			if (par.contains("min") || par.contains("max")) {
-				boolean minB = par.lastIndexOf("min") > par.lastIndexOf("max") || !par.contains("max");
-				if (minB) {
-					String[] min = par.substring(par.lastIndexOf("min(") + 4, par.indexOf(")", par.lastIndexOf("min("))).split(",");
-					float[] minf = new float[2];
-					for (int i = 0; i < min.length; i++) {
-						String minSt = min[i];
-						while (minSt.contains("(") || minSt.contains("[") || minSt.contains("<") || minSt.contains("{")) {
-							minSt = simplifyParenthesis(minSt, recursion + 1);
-						}
-						minf[i] = solveInner(minSt);
-					}
-					par = par.replace("min(" + min[0] + "," + min[1] + ")", String.valueOf(Math.min(minf[0], minf[1])));
-				} else {
-					String[] max = par.substring(par.lastIndexOf("max(") + 4, par.indexOf(")", par.lastIndexOf("max("))).split(",");
-					float[] maxf = new float[2];
-					for (int i = 0; i < max.length; i++) {
-						String maxSt = max[i];
-						while (maxSt.contains("(") || maxSt.contains("[") || maxSt.contains("<") || maxSt.contains("{")) {
-							maxSt = simplifyParenthesis(maxSt, recursion + 1);
-						}
-						maxf[i] = solveInner(maxSt);
-					}
-					par = par.replace("max(" + max[0] + "," + max[1] + ")", String.valueOf(Math.max(maxf[0], maxf[1])));
-				}
-			} else if (par.contains("pow")) {
-				String[] pow = par.substring(par.lastIndexOf("pow<") + 4, par.indexOf(">", par.lastIndexOf("pow<"))).split("\\^");
-				float[] powf = new float[2];
-				for (int i = 0; i < pow.length; i++) {
-					String powSt = pow[i];
-					while (powSt.contains("(") || powSt.contains("[") || powSt.contains("<") || powSt.contains("{")) {
-						powSt = simplifyParenthesis(powSt, recursion + 1);
-					}
-					powf[i] = solveInner(powSt);
-				}
-				par = par.replace("pow<" + pow[0] + "^" + pow[1] + ">", String.valueOf(Math.pow(powf[0], powf[1])));
-			} else if (par.contains("mul") || par.contains("div")) {
-				boolean mulB = par.lastIndexOf("mul") > par.lastIndexOf("div") || !par.contains("div");
-				if (mulB) {
-					String[] mul = par.substring(par.lastIndexOf("mul[") + 4, par.indexOf("]", par.lastIndexOf("mul["))).split("\\*");
-					float[] mulf = new float[2];
-					for (int i = 0; i < mul.length; i++) {
-						String mulSt = mul[i];
-						while (mulSt.contains("(") || mulSt.contains("[") || mulSt.contains("<") || mulSt.contains("{")) {
-							mulSt = simplifyParenthesis(mulSt, recursion + 1);
-						}
-						mulf[i] = solveInner(mulSt);
-					}
-					par = par.replace("mul[" + mul[0] + "*" + mul[1] + "]", String.valueOf(mulf[0] * mulf[1]));
-				} else {
-					String[] div = par.substring(par.lastIndexOf("div[") + 4, par.indexOf("]", par.lastIndexOf("div["))).split("/");
-					float[] divf = new float[2];
-					for (int i = 0; i < div.length; i++) {
-						String divSt = div[i];
-						while (divSt.contains("(") || divSt.contains("[") || divSt.contains("<") || divSt.contains("{")) {
-							divSt = simplifyParenthesis(divSt, recursion + 1);
-						}
-						divf[i] = solveInner(divSt);
-					}
-					par = par.replace("div[" + div[0] + "/" + div[1] + "]", String.valueOf(divf[0] / divf[1]));
-				}
-			} else if (par.contains("add") || par.contains("sub")) {
-				boolean addB = par.lastIndexOf("add") > par.lastIndexOf("sub") || !par.contains("sub");
-				if (addB) {
-					String[] add = par.substring(par.lastIndexOf("add{") + 4, par.indexOf("}", par.lastIndexOf("add{"))).split("\\+");
-					float[] addf = new float[2];
-					for (int i = 0; i < add.length; i++) {
-						String addSt = add[i];
-						while (addSt.contains("(") || addSt.contains("[") || addSt.contains("<") || addSt.contains("{")) {
-							addSt = simplifyParenthesis(addSt, recursion + 1);
-						}
-						addf[i] = solveInner(addSt);
-					}
-					par = par.replace("add{" + add[0] + "+" + add[1] + "}", String.valueOf(addf[0] + addf[1]));
-				} else {
-					String[] sub = par.substring(par.lastIndexOf("sub{") + 4, par.indexOf("}", par.lastIndexOf("sub{"))).split("-");
-					float[] subf = new float[2];
-					for (int i = 0; i < sub.length; i++) {
-						String subSt = sub[i];
-						while (subSt.contains("(") || subSt.contains("[") || subSt.contains("<") || subSt.contains("{")) {
-							subSt = simplifyParenthesis(subSt, recursion + 1);
-						}
-						subf[i] = solveInner(subSt);
-					}
-					par = par.replace("sub{" + sub[0] + "-" + sub[1] + "}", String.valueOf(subf[0] - subf[1]));
-				}
-			}
-			return par;
-		}
-		public float solveInner(String par) {
-			return Float.parseFloat(par);
-		}
-		public static int min(int val, int comp) {
-			if (comp == -1 || comp == 0)
-				comp = Integer.MAX_VALUE;
-			if (val == -1 || val == 0)
-				val = Integer.MAX_VALUE;
-			return Math.min(val, comp);
 		}
 	}
 }
