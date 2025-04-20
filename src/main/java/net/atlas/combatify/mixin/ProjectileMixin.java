2f87656167d0b03ebdd4b1c4c04c94e09d59de31
Alexandra-Myers <bia416cat@gmail.com>
2025-04-19 23:38:44 -0400

    Combatify 1.21.4 1.2.7 Fabric - ...

--- src/main/java/net/atlas/combatify/mixin/ProjectileMixin.java
+++ src/main/java/net/atlas/combatify/mixin/ProjectileMixin.java
@@ -22,7 +22,7 @@ public abstract class ProjectileMixin extends Entity {
 	public void redirectShoot(Entity user, float pitch, float yaw, float roll, float modifierZ, float modifierXYZ, CallbackInfo ci) {
 		if (Combatify.CONFIG.ctsMomentumPassedToProjectiles()) {
 			Vec3 currentMomentum = getDeltaMovement();
-			Vec3 entityMomentum = user.onGround() ? user.getKnownMovement() : user.getKnownMovement().multiply(1.0, 0.0, 1.0);
+			Vec3 entityMomentum = user.onGround() ? user.getDeltaMovement() : user.getDeltaMovement().multiply(1.0, 0.0, 1.0);
 			Vec3 projected = MethodHandler.project(entityMomentum, currentMomentum);
 			setDeltaMovement(currentMomentum.add(projected));
 			ci.cancel();
