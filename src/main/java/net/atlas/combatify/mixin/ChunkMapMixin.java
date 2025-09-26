e3575a2272622e949f41d419d7db7c6f5b2e1e42
Alexandra-Myers <bia416cat@gmail.com>
2025-09-26 16:08:47 -0400

    Combatify 1.21.8 1.2.10 Fabric - remove annotation

--- src/main/java/net/atlas/combatify/mixin/ChunkMapMixin.java
+++ src/main/java/net/atlas/combatify/mixin/ChunkMapMixin.java
@@ -13,7 +13,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Incompatible("vmp")
 @Mixin(ChunkMap.class)
 public abstract class ChunkMapMixin implements ChunkMapExtensions {
 	@Shadow
