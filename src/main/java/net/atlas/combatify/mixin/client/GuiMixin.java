9acaed5726a39e7fcb8174a36416abdbb560a845
Alexandra-Myers <bia416cat@gmail.com>
2025-10-12 20:07:46 -0400

    Combatify 25w41a 1.2.13 Fabric - Boom

--- src/main/java/net/atlas/combatify/mixin/client/GuiMixin.java
+++ src/main/java/net/atlas/combatify/mixin/client/GuiMixin.java
@@ -296,7 +296,13 @@ public abstract class GuiMixin {
 				float power = BowItem.getPowerForTime(time);
 				int xPos = mutableXPos.getAndAdd(right ? -20 : 20);
 				if (power < 1) renderHotbarProgress(guiGraphics, xPos, yPos, power);
-				else if (getFatigueForTime(time) <= 0.5f) guiGraphics.blitSprite(RenderPipelines.GUI_TEXTURED, HOTBAR_ATTACK_INDICATOR_FULL_SPRITE, xPos, yPos, 18, 18);
+				else if (getFatigueForTime(time) <= 0.5f) {
+					float fatigueProgress = 1 - ((float) (time - 20) / 40);
+					renderHotbarProgress(guiGraphics, xPos, yPos, fatigueProgress);
+					int indicatorXPos = xPos + 1;
+					int indicatorYPos = yPos + 1;
+					guiGraphics.blitSprite(RenderPipelines.GUI_TEXTURED, HOTBAR_SPECIAL_INDICATOR_FULL_SPRITE, indicatorXPos, indicatorYPos, 5, 5);
+				}
 			}
 			case CrossbowItem ignored -> {
 				float power = (float) time / CrossbowItem.getChargeDuration(useItem, player);
@@ -322,7 +328,13 @@ public abstract class GuiMixin {
 				float power = BowItem.getPowerForTime(time);
 				int yPos = mutableYPos.getAndAdd(8);
 				if (power < 1) renderCrosshairProgress(guiGraphics, xPos, yPos, power);
-				else if (getFatigueForTime(time) <= 0.5f) guiGraphics.blitSprite(RenderPipelines.CROSSHAIR, CROSSHAIR_ATTACK_INDICATOR_FULL_SPRITE, xPos, yPos, 16, 16);
+				else if (getFatigueForTime(time) <= 0.5f) {
+					float fatigueProgress = 1 - ((float) (time - 20) / 40);
+					renderCrosshairProgress(guiGraphics, xPos, yPos, fatigueProgress);
+					int indicatorXPos = guiGraphics.guiWidth() / 2 - 1;
+					int indicatorYPos = yPos + 4;
+					guiGraphics.blitSprite(RenderPipelines.CROSSHAIR, CROSSHAIR_SPECIAL_INDICATOR_FULL_SPRITE, indicatorXPos, indicatorYPos, 3, 3);
+				}
 			}
 			case CrossbowItem ignored -> {
 				float power = (float) time / CrossbowItem.getChargeDuration(useItem, player);
