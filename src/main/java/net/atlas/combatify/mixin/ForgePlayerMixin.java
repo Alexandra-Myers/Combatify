b8ed8ac80022ee8d48fa09d76f44d7fc0638383a
Alexandra-Myers <bia416cat@gmail.com>
2025-10-28 13:04:50 -0400

    Combatify 1.20.1 1.1.8.6 Forge - whoops

--- src/main/java/net/atlas/combatify/mixin/ForgePlayerMixin.java
+++ src/main/java/net/atlas/combatify/mixin/ForgePlayerMixin.java
@@ -1,20 +1,23 @@
 package net.atlas.combatify.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.atlas.combatify.util.MethodHandler;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.extensions.IForgePlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(IForgePlayer.class)
+@Mixin(value = IForgePlayer.class, remap = false)
 public interface ForgePlayerMixin {
-	@Shadow
-	Player self();
+    @Shadow
+    Player self();
 
-	@WrapMethod(method = "getEntityReach")
-	default double getEntityReach(Operation<Double> original) {
+    /**
+     * @author Alexandra
+     * @reason Can't use anything in this economy
+     */
+    @Overwrite()
+    default double getEntityReach() {
 		return MethodHandler.getCurrentAttackReach(self(),0.0F);
 	}
 }
