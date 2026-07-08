d3b20716ef8329a0fdd7302dff40b9f73c594f80
pigsteel <43256029+pigsteel@users.noreply.github.com>
2026-07-08 14:49:46 -0400

    Combatify Multicutter - Redundant platform impl and separation of platform initializers

---
+++ src/main/java/net/atlas/combatify/neoforge/CombatifyNeoForgePlatform.java
@@ -0,0 +1,8 @@
+package net.atlas.combatify.neoforge;
+
+//? neoforge {
+/*import net.atlas.combatify.CombatifyPlatform;
+
+public class CombatifyNeoForgePlatform implements CombatifyPlatform {
+}
+*///?}
