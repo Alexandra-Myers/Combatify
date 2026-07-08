f77225c859da53fa1a6b72e9fe2d5b4ada4a5258
pigsteel <43256029+pigsteel@users.noreply.github.com>
2026-07-08 16:20:09 -0400

    Combatify Multicutter - I renamed HeptaConsumer to SeptaConsumer and all I got was this stupid T-shirt

--- src/main/java/net/atlas/combatify/util/SeptaConsumer.java
+++ src/main/java/net/atlas/combatify/util/SeptaConsumer.java
@@ -1,10 +1,10 @@
 package net.atlas.combatify.util;
 
 @FunctionalInterface
-public interface HeptaConsumer<H, I, J, K, L, M, N> {
+public interface SeptaConsumer<H, I, J, K, L, M, N> {
     void accept(H var1, I var2, J var3, K var4, L var5, M var6, N var7);
 
-	default HeptaConsumer<H, I, J, K, L, M, N> andThen(HeptaConsumer<? super H, ? super I, ? super J, ? super K, ? super L, ? super M, ? super N> after) {
+	default SeptaConsumer<H, I, J, K, L, M, N> andThen(SeptaConsumer<? super H, ? super I, ? super J, ? super K, ? super L, ? super M, ? super N> after) {
 		return (h, i, j, k, l, m, n) -> {
 			this.accept(h, i, j, k, l, m, n);
 			after.accept(h, i, j, k, l, m, n);
