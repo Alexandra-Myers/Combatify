0f5f5292bab2613fc00d1837daa939a5a7acd563
pigsteel <43256029+pigsteel@users.noreply.github.com>
2026-07-08 14:50:03 -0400

    Combatify Multicutter - Last one pt.2 (WIP)

---
+++ src/main/java/net/atlas/combatify/neoforge/CombatifyNeoForge.java
@@ -0,0 +1,21 @@
+package net.atlas.combatify.neoforge;
+
+//? neoforge {
+
+
+/*import net.atlas.combatify.Combatify;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+
+
+@Mod(Combatify.MOD_ID)
+@EventBusSubscriber(modid = Combatify.MOD_ID)
+public class CombatifyNeoForge {
+
+	@Override
+	public void onInitialize() {
+		Combatify.CLEANER.register(this, Context::exit);
+		Combatify.init();
+	}
+}
+*///?}
