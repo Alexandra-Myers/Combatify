5ff864fed2c3cd110bcc71c2bc1ae3260e2e00b0
pigsteel <43256029+pigsteel@users.noreply.github.com>
2026-07-08 16:18:49 -0400

    Combatify Multicutter - HeptaConsumer interface and KnockbackMode fixed

--- src/main/java/net/atlas/combatify/config/KnockbackMode.java
+++ src/main/java/net/atlas/combatify/config/KnockbackMode.java
@@ -1,6 +1,7 @@
 package net.atlas.combatify.config;
 
 import net.atlas.combatify.Combatify;
+import net.atlas.combatify.util.HeptaConsumer;
 import net.atlas.combatify.util.MethodHandler;
 import net.atlas.combatify.util.QuadConsumer;
 import net.minecraft.tags.DamageTypeTags;
@@ -16,7 +17,19 @@ public enum KnockbackMode {
 	CTS_8C,
 	CTS_5,
 	MIDAIR;
-	public void runKnockback(LivingEntity target, @Nullable DamageSource source, double strength, double x, double z, QuadConsumer<LivingEntity, Double, Double, Double> vanillaCall) {
+	public void runKnockback(
+		LivingEntity target,
+		@Nullable DamageSource source,
+		double strength,
+		double x,
+		double z
+		//? <26.2 {
+		/*, QuadConsumer<LivingEntity, Double, Double, Double> vanillaCall
+		*///?} >=26.2 {
+		, HeptaConsumer<LivingEntity, Double, Double, Double, DamageSource, Float, Boolean> vanillaCall,
+		float damage
+		//?}
+	) {
 		boolean applyNonProjectileKB = false;
 		if (this == MIDAIR && source == null) applyNonProjectileKB = true;
 		else if (source != null) applyNonProjectileKB = (Combatify.CONFIG.fishingHookKB() && source.getDirectEntity() instanceof FishingHook);
@@ -30,7 +43,11 @@ public enum KnockbackMode {
 				else MethodHandler.knockback(target, strength, x, z);
 			}
 			case CTS_5 -> MethodHandler.combatTest5Knockback(target, strength, x, z);
-			case VANILLA -> vanillaCall.accept(target, strength, x, z);
+			//? <26.2 {
+			/*case VANILLA -> vanillaCall.accept(target, strength, x, z);
+			*///?} >=26.2 {
+			case VANILLA -> vanillaCall.accept(target, strength, x, z, source, damage, false);
+			//?}
 			case OLD -> MethodHandler.oldKnockback(target, strength, x, z);
 		}
 	}
